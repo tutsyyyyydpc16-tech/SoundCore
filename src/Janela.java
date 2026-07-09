5fda338fdfd553fa0d5d16adfe70bfd5bbf7ccef
Arthur <tutsyyyyy.dpc16@gmail.com>
2026-07-09 16:36:25 -0500

    Adicionado botões e escolher músicas

--- src/Janela.java
+++ src/Janela.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.io.File;
 
 public class Janela extends JFrame {
 
@@ -8,7 +11,81 @@ public class Janela extends JFrame {
         setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Controles();
+        PainelCentral();
+
         setVisible(true);
 
     }
+
+    public void Controles() {
+
+        JPanel control = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        JButton abrirMusica = new JButton("Abrir música");
+        control.add(abrirMusica);
+        abrirMusica.addActionListener(e -> AbrirMusica());
+
+        JButton anterior = new JButton("<<");
+        control.add(anterior);
+
+        JButton play = new JButton("▶ Play");
+        control.add(play);
+
+        JButton seguinte = new JButton(">>");
+        control.add(seguinte);
+
+        add(control, BorderLayout.SOUTH);
+
+        control.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+    }
+
+    public void PainelCentral() {
+
+        JPanel central = new JPanel();
+        central.setLayout(new GridLayout(3, 1));
+
+
+        capa = new JLabel("[Nenhuma capa]");
+        musica = new JLabel("Nenhuma música");
+        artista = new JLabel("Nenhum artista");
+
+        central.add(capa);
+        central.add(musica);
+        central.add(artista);
+
+        add(central, BorderLayout.CENTER);
+
+        central.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+    }
+
+    public void AbrirMusica() {
+
+        JFileChooser musicas = new JFileChooser();
+        musicas.setCurrentDirectory(new File("."));
+
+        FileNameExtensionFilter filtros = new FileNameExtensionFilter("Músicas (*.mp3)",
+                "mp3");
+
+        musicas.setFileFilter(filtros);
+        musicas.setAcceptAllFileFilterUsed(false);
+
+        int result = musicas.showOpenDialog(this);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+
+            caminhoMusica = musicas.getSelectedFile().getPath();
+        }
+
+    }
+
+    private JLabel musica;
+    private JLabel artista;
+    private JLabel capa;
+    private File musicaAtual;
+    private String caminhoMusica;
+
 }
