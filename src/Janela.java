32ba7edfcb52485a3120d7c4bc4f1413c6f624ed
Arthur <tutsyyyyy.dpc16@gmail.com>
2026-07-09 15:33:20 -0500

    Adicionado uma classe para janela

---
+++ src/Janela.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Janela extends JFrame {
+
+    public Janela() {
+
+        setTitle("SoundCore");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,6 @@ public class Main {
 
     public static void main (String[] args) {
 
-        Janela();
-    }
-
-    public static void Janela() {
-
-        JFrame janela = new JFrame();
-
-        janela.setSize(800, 600);
-        janela.setLocationRelativeTo(null);
-        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        janela.setVisible(true);
+        new Janela();
     }
 }
